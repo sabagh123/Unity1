908a4e05bcd6da66d45ef0fa8f02d7d682e047ad
choladzeGiorgi <154664565+choladzeGiorgi@users.noreply.github.com>
2025-04-24 09:03:26 -0700

    init

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TextChanger : MonoBehaviour
+{
+    [SerializeField] private GameObject startMenuPanel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
