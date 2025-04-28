4f3d7c041813b3dc43a7ff8c51e8d63e523f09a1
choladzeGiorgi <154664565+choladzeGiorgi@users.noreply.github.com>
2025-04-28 08:13:02 -0700

    init

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -1,16 +1,41 @@
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+
 
 public class TextChanger : MonoBehaviour
 {
-    [SerializeField] private GameObject startMenuPanel;
-    void Start()
+    public Button btn1;
+    public TMP_Text label;
+    public Button btn2;
+    
+    private void Start()
     {
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+
+        btn1.onClick.AddListener(() => 
+        {
+            label.text = "2";
+            label.color = Color.red;
+            
+            }); 
         
+        btn2.onClick.AddListener(() => {
+            int count;
+            if(int.TryParse(label.text,out count))
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+            label.text =count.ToString();
+            label.color = Color.green;
+            
+            
+            }
+        );
     }
 }
